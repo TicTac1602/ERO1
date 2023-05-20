7066ee0b605917c3a8243443d4dbdcc365fe1ffc
Emil Toulouse <emil.toulouse@epita.fr>
2023-05-20 11:32:18 +0200

    first version of the eulerian cycle

--- reconaissance/scan.py
+++ reconaissance/scan.py
@@ -20,12 +20,12 @@ def scan(place_name, verbose=False):
         place_name = "Rivière-des-Prairies-Pointe-aux-Trembles, Montreal, Canada"
         pass
     else:
-        print(place_name, "n'est pas un emplacement gerer dans ce projet")
+        print(place_name, " n'est pas un emplacement gerer dans ce projet")
         print("Les quartiers gerer sont : [outremont,verdun,montRoyal,riviere,saintLeonard]")
         return
     # Télécharger le graphe du réseau de rues du lieu spécifié
     graph = ox.graph_from_place(place_name, network_type="drive")
-    G = ox.plot_graph(graph, show=verbose)
+    ox.plot_graph(graph, show=verbose)
     GU = graph.to_undirected()
     if not nx.is_weakly_connected(graph):
         print("Le graphe n'est pas connexe.")
@@ -42,10 +42,10 @@ def scan(place_name, verbose=False):
 
     if verbose:
         print("Chemin calculer")
-        for u, v,data in graph.edges(data=True):
-            print(data['length'])
-
-        print("Taille du réseau routier : ", real_distance, "m")
+        reel_distance = 0
+        for u,v,data in graph.edges(data=True):
+            reel_distance += data['length']
+        print("Surface reel du quartier:", reel_distance, 'm')
         print("Distance totale parcourue :", distance_totale, "m")
         print("Creation du plan avec l'itineraire (Cette operation peut etre longue) ETA :" , 0.08 * len(chemin_parcouru), " secondes")
 
@@ -58,81 +58,48 @@ def scan(place_name, verbose=False):
     print("Fin du scan")
     return chemin_parcouru, distance_totale
 
+def dfs(graph, node, visited):
+    stack = [(node, 0, [node])]  
+    while stack:
+        current_node, distance, path = stack.pop()  
+        visited.add(current_node)  # Marquer le nœud comme visité
+        if len(visited) > 1 and graph.degree(current_node) % 2 != 0:
+            return current_node, distance, path  # Retourner le nœud, la distance et le chemin
+        for neighbor in graph.neighbors(current_node):
+            if neighbor not in visited:
+                edge_length = graph.get_edge_data(current_node, neighbor)[0]['length']
+                stack.append((neighbor, distance + edge_length, path + [neighbor]))
+
 def make_it_eulerian(graph):
     odd_nodes = [node for node in graph.nodes if graph.degree(node) % 2 != 0]
-    
-    # Transformer le graph pour ajouter les arcs pour que ce soit un graphe avec que des degres pair
-    
+    added_edge = 0
+    while(odd_nodes):
+        node = odd_nodes[0]
+        neighbors = list(graph.neighbors(node))
+        odd_degree_neighbors = [neighbor for neighbor in graph.neighbors(node) if graph.degree(neighbor) % 2 != 0]
+        if(odd_degree_neighbors):
+            length = graph.get_edge_data(odd_degree_neighbors[0], node)[0]['length']
+            graph.add_edge(node, odd_degree_neighbors[0],length=length)
+            odd_nodes = [node for node in graph.nodes if graph.degree(node) % 2 != 0]
+            added_edge +=1
+        else :
+            # faire un DFS qui retourne le premier noeud pas visiter de degres imp et la distance pour aller a ce point
+            visited = set()
+            target_node, distance, path = dfs(graph, node, visited)
+            for i in range(len(path) - 1):
+                source = path[i]
+                destination = path[i + 1]
+                edge_length = graph.get_edge_data(source, destination)[0]['length']
+                graph.add_edge(source, destination, length=edge_length)
+            odd_nodes = [node for node in graph.nodes if graph.degree(node) % 2 != 0]
+            added_edge +=1
     odd_nodes = [node for node in graph.nodes if graph.degree(node) % 2 != 0]
     if not odd_nodes:
-        eulerian_cycle = find_eulerian_circuit(len(graph.nodes()),graph.edges(data=True))
-        distance = sum(graph[u][v]['length'] for u, v in eulerian_cycle)
+        eulerian_cycle, distance = parcourir_aretes_euler(graph)
         return True, eulerian_cycle, distance
     else:
         print("Pas de cycle eulerien apres ajout d'arc")
         return False, None, 0
-
-def odd_vertices(n, edges):
-    deg = [0] * n
-    for (a,b) in edges:
-        deg[a] += 1
-        deg[b] += 1
-    return [a for a in range(n) if deg[a] % 2]
-
-def is_edge_connected(n, edges):
-    if n == 0 or len(edges) == 0:
-        return True
-    # Convert to adjacency list
-    succ = [[] for a in range(n)]
-    for (a,b) in edges:
-        succ[a].append(b)
-        succ[b].append(a)
-    # BFS over the graph, starting from one extremity of the first edge
-    touched = [False] * n
-    init = edges[0][0]
-    touched[init] = True
-    todo = [init]
-    while todo:
-        s = todo.pop()
-        for d in succ[s]:
-            if touched[d]:
-                continue
-            touched[d] = True
-            todo.append(d)
-    for a in range(n):
-        if succ[a] and not touched[a]:
-            return False
-    return True
-
-def is_eulerian(n, edges):
-    return is_edge_connected(n, edges) and not odd_vertices(n, edges)
-
-def find_eulerian_circuit(n, edges):
-    assert is_eulerian(n, edges)
-    if len(edges) == 0:
-        return []
-    cycle = [edges[0][0]] # start somewhere
-    while True:
-        rest = []
-        for (a, b) in edges:
-            if cycle[-1] == a:
-                cycle.append(b)
-            elif cycle[-1] == b:
-                cycle.append(a)
-            else:
-                rest.append((a,b))
-        if not rest:
-            assert cycle[0] == cycle[-1]
-            return cycle[0:-1]
-        edges = rest
-        if cycle[0] == cycle[-1]:
-            # Rotate the cycle so that the last state
-            # has some outgoing edge in EDGES.
-            for (a, b) in edges:
-                if a in cycle:
-                    idx = cycle.index(a)
-                    cycle = cycle[idx:-1] + cycle[0:idx+1]
-                    break
 
 # Parcours de toutes les arêtes du graphe
 def parcourir_aretes(graph):
@@ -146,3 +113,10 @@ def parcourir_aretes(graph):
 
     return chemin, distance_totale
 
+def parcourir_aretes_euler(graph):
+    if not nx.is_eulerian(graph):
+        return "Le graphe n'est pas eulérien."
+    cycle = nx.eulerian_circuit(graph)
+    cycle_arretes = [[u, v] for u, v in cycle]
+    distance_totale = sum(graph.get_edge_data(u, v)[0]['length'] for u, v in cycle_arretes)
+    return cycle_arretes,distance_totale
