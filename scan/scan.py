f5832b289874e85a30228563f4d8a79d244c8d91
Emil Toulouse <emil.toulouse@epita.fr>
2023-05-29 13:34:19 +0200

    change on scan with optimisation and cleaner file

--- scan/scan.py
+++ scan/scan.py
@@ -1,7 +1,8 @@
-import osmnx as ox
-import networkx as nx
 from datetime import datetime
+import heapq
 import math
+import networkx as nx
+import osmnx as ox
 import time
 
 # Dictionnaire des correspondances entre les noms de lieux et leurs emplacements
@@ -14,11 +15,20 @@ lieux = {
     "saintLeonard": "Saint-Léonard, Montreal, Canada"
 }
 
-drone_speed = 40
+drone_speed = 70
 drone_cost_per_km = 0.01
 drone_cost_daily = 100
 
 def scan(place_name, verbose=False):
+    """
+    Effectue un scan d'une région spécifiée et affiche les informations relatives au parcours.
+
+    @param place_name: Nom de la région à scanner.
+    @param verbose: Indicateur pour l'affichage détaillé, par défaut False.
+    @return: Tuple contenant le chemin parcouru (liste d'arêtes) et la distance totale parcourue (float).
+    """
+
+    #Verification des arguments
     if place_name in lieux:
         place_name = lieux[place_name]
     else:
@@ -26,21 +36,26 @@ def scan(place_name, verbose=False):
         print("Les quartiers gérés sont : [montreal,outremont,verdun,montRoyal,riviere,saintLeonard]")
         return
     print(datetime.now().strftime("[%d/%m %H:%M:%S]"), "Debut du scan de la région :",place_name)
+    
     # Télécharger le graphe du réseau de rues du lieu spécifié
     graphique = ox.graph_from_place(place_name, network_type="drive")
     ox.plot_graph(graphique, show=verbose)
     GU = graphique.to_undirected()
     if not nx.is_connected(GU):
         print("Le graphe n'est pas connexe.")
+    
     print(datetime.now().strftime("[%d/%m %H:%M:%S]"), "Graphique de la région",place_name, "chargé !")
+    
+    #Situation initale
     chemin_parcouru = None
     distance_totale = None
     reel_distance = 0
     for u, v, data in graphique.edges(data=True):
         reel_distance += data["length"]
     print(datetime.now().strftime("[%d/%m %H:%M:%S]"), "Surface réelle du quartier :",round(reel_distance/1000,2), "km")
-    start = time.time()
+    
     # Parcours des arêtes et calcul de la distance totale
+    start = time.time()
     found, chemin_parcouru, distance_totale = make_it_eulerian(GU,verbose)
     if not found:
         print("Calcul du chemin en glouton")
@@ -48,26 +63,34 @@ def scan(place_name, verbose=False):
     end = time.time()
 
     # Affichage des informations
-    
-    time_travel = (distance_totale/1000)/40
+    time_travel = (distance_totale/1000)/drone_speed
     daily_cost = math.ceil(time_travel/24)
 
     print(datetime.now().strftime("[%d/%m %H:%M:%S]"),"Chemin trouvé en",round(end - start,4), "s")
     print(datetime.now().strftime("[%d/%m %H:%M:%S]"), "Distance totale parcourue :",round(distance_totale/1000,2), "km")
+    print(datetime.now().strftime("[%d/%m %H:%M:%S]"),"Temps estimé du parcours :",round(time_travel,2) ,"h")
+    print(datetime.now().strftime("[%d/%m %H:%M:%S]"), "Cout estimé du scan : ",(daily_cost*100 + math.ceil(distance_totale/1000)*0.01) ,"€")
 
     if verbose:
         print(datetime.now().strftime("[%d/%m %H:%M:%S]"), "Création du plan avec l'itinéraire (Cette opération peut être longue) ETA :",0.08 * len(chemin_parcouru),"secondes")
         # Tracer l'itinéraire sur OSMnx
         ox.plot_graph_routes(GU, chemin_parcouru, route_linewidth=6, node_size=0, bgcolor="w", show=True)
 
-    print(datetime.now().strftime("[%d/%m %H:%M:%S]"),"Temps estimé du parcours :",round(time_travel,2) ,"h")
-    print(datetime.now().strftime("[%d/%m %H:%M:%S]"), "Cout estimé du scan : ",(daily_cost*100 + math.ceil(distance_totale/1000)*0.01) ,"€")
     print(datetime.now().strftime("[%d/%m %H:%M:%S]"), "Fin du scan")
 
     return chemin_parcouru, distance_totale
 
+def bfs_min(graph, node, visited):
+    """
+    Effectue un parcours en largeur (BFS) à partir d'un nœud donné dans le graphe.
 
-def dfs_min(graph, node, visited):
+    @param graph: Graphe à parcourir.
+    @param node: Nœud de départ du parcours.
+    @param visited: Ensemble des nœuds visités.
+    @return: Tuple contenant le nœud de degré impair le plus proche, la distance pour y arriver et le chemin parcouru.
+    """
+
+    # Initialisation des paramètres
     stack = [(node, 0, [node])]
     min_dist = float('inf')
     better = (None, float('inf'), None)
@@ -84,9 +107,46 @@ def dfs_min(graph, node, visited):
                 edge_length = graph.get_edge_data(current_node, neighbor)[0]["length"]
                 stack.append((neighbor, distance + edge_length, path + [neighbor]))
     return better
-    
+
+def dijkstra(graph, node,visited):
+    """
+    Effectue l'algorithme de Dijkstra à partir d'un nœud donné dans le graphe.
+
+    @param graph: Graphe à parcourir.
+    @param node: Nœud de départ du parcours.
+    @param visited: Ensemble des nœuds visités.
+    @return: Tuple contenant le nœud de degré impair ayant la plus petite distance, la distance pour y arriver et le chemin parcouru.
+    """
+
+    heap = []
+    heapq.heappush(heap, (0, node, [node]))
+    min_dist = float('inf')
+    better = (None, float('inf'), None)
+    while heap:
+        distance, current_node, path = heapq.heappop(heap)
+        if(current_node in visited):
+            continue
+        visited.add(current_node)
+        if len(visited) > 1 and graph.degree(current_node) % 2 != 0:
+            # Sélection du nœud de degré impair ayant la plus petite distance
+            if distance < min_dist:
+                min_dist = distance
+                better = (current_node, distance, path)
+        for neighbor in graph.neighbors(current_node):
+            if neighbor not in visited:
+                edge_length = graph.get_edge_data(current_node, neighbor)[0]["length"]
+                heapq.heappush(heap, (distance + edge_length, neighbor, path + [neighbor]))
+    return better    
 
 def make_it_eulerian(graph,verbose):
+    """
+    Transforme le graphe en un graphe eulérien en ajoutant des arêtes supplémentaires.
+
+    @param graph: Graphe à transformer en graphe eulérien.
+    @param verbose: Indicateur pour l'affichage détaillé.
+    @return: Tuple contenant un indicateur si un chemin eulérien a été trouvé, le chemin parcouru (liste d'arêtes) et la distance totale parcourue (float).
+    """
+
     odd_nodes = [node for node in graph.nodes if graph.degree(node) % 2 != 0]
     nb_todo = len(odd_nodes)
     while odd_nodes:
@@ -96,9 +156,9 @@ def make_it_eulerian(graph,verbose):
             length = graph.get_edge_data(odd_degree_neighbors[0], node)[0]["length"]
             graph.add_edge(node, odd_degree_neighbors[0], directed=False, length=length)
         else:
-            # Faire un DFS qui retourne le premier nœud non visité de degré impair et la distance pour y arriver
             visited = set()
-            target_node, distance, path = dfs_min(graph, node, visited)
+            # - opti mais fonctionne : target_node, distance, path = bfs_min(graph, node, visited)
+            target_node,distance,path = dijkstra(graph, node,visited)
             # Création d'arcs pour atteindre ce nœud
             for i in range(len(path) - 1):
                 source = path[i]
@@ -114,41 +174,33 @@ def make_it_eulerian(graph,verbose):
     print(datetime.now().strftime("[%d/%m %H:%M:%S]"), "Un itinéraire a été trouvé")
     return True, eulerian_cycle, distance
 
-
-# Parcours de toutes les arêtes du graphe
-def parcourir_aretes(graph):
-    chemin = []
-    distance_totale = 0  # Variable pour stocker la distance totale parcourue
-    for u, v, k, data in graph.edges(keys=True, data=True):
-        chemin.append([u, v])
-        distance = data["length"]
-        distance_totale += distance  # Ajouter la distance
-        chemin.append([v, u])
-
-    return chemin, distance_totale
-
-
 def parcourir_aretes_euler(graph,verbose):
+    """
+    Parcourt les arêtes du graphe eulérien pour former un cycle eulérien.
+
+    @param graph: Graphe eulérien à parcourir.
+    @param verbose: Indicateur pour l'affichage détaillé.
+    @return: Tuple contenant la liste des arêtes parcourues dans le cycle eulérien et la distance totale parcourue (float).
+    """
+
     if not nx.is_eulerian(graph):
         return None,None
     cycle = find_eulerian_cycle(graph)
-    # Pretty print of the cycle
-    # if not verbose:
-    #     for i in range(len(cycle) - 1, -1, -1):
-    #         print(cycle[i], end = "")
-    #         if i:
-    #             print(" -> ", end = "")
-    #     print("")
     list = [[cycle[i], cycle[i+1]] for i in range(len(cycle)-1)]
     distance_totale = sum(graph.get_edge_data(u, v)[0]["length"] for u, v in list)
     return list, distance_totale
 
 def find_eulerian_cycle(graph):
+    """
+    Trouve un cycle eulérien dans le graphe.
 
-    # Vérifier si le graphe est eulérien
+    @param graph: Graphe dans lequel chercher un cycle eulérien.
+    @return: Liste des nœuds parcourus dans le cycle eulérien.
+    """
+
     if not nx.is_eulerian(graph):
         return None
-    
+
     # Copier le graphe pour effectuer les modifications
     modified_graph = graph.copy()
     
