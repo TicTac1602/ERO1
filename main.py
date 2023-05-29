85517006fc77172913ca78f267f213d9ab524388
Emil Toulouse <emil.toulouse@epita.fr>
2023-05-29 13:34:45 +0200

    updated main

--- main.py
+++ main.py
@@ -8,19 +8,17 @@ def run_scenario(scenario, place):
     print(f'Lancement du scenario {scenario} à {place}')
     if scenario == "scan":
         if place:
-            scan(place)
-        else:
-            scan("montreal")
+            return scan(place)
 
 def main():
     parser = argparse.ArgumentParser(description='ERO1 Optimisation Hivernale')
-    parser.add_argument('--scenario', choices=['scan', 'scenario2', 'scenario3'], default='scan', help='Le scénario à exécuter')
+    parser.add_argument('--scenario', choices=['scan', 'scenario2', 'scenario3'], default='scan', help='Le scénario à exécuter (par défaut: scan)')
     parser.add_argument('--place', choices=['montreal', 'outremont', 'verdun', 'riviere', 'saintLeonard', 'montRoyal'], default='montreal', help="Choix de l'endroit ou effectuer le scenario (par défaut: montreal)")
 
     args = parser.parse_args()
 
     if args.scenario == 'scan':
-        run_scenario(args.scenario, args.place)
+        return run_scenario(args.scenario, args.place)
     elif args.scenario == 'scenario2':
         run_scenario(args.scenario, None)  # Ajouter un cas si nécessaire
     elif args.scenario == 'scenario3':
