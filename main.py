d77d180484d1e565e8ccc9dd497b6cd755331b00
Emil Toulouse <emil.toulouse@epita.fr>
2023-06-03 18:00:29 +0200

    Updated main

--- main.py
+++ main.py
@@ -4,23 +4,43 @@ import sys
 sys.path.append('./deneigement')
 sys.path.append('./scan')
 from scan import scan
-from deneigement import deneigement_euler
+from deneigement.deneigement_euler import deneigement_euler,calculer_meilleur_choix
 
-def run_scenario(scenario, place, multi=False):
-    print(f'Lancement du scenario {scenario} à {place}')
+deneigeuse_T1 = {
+    "nom" : "deneigeuse Type1",
+    "vitesse" :  10,
+    "cout_jour": 500,
+    "cout_kilo": 1.1,
+    "cout_horaires_8":1.1,
+    "cout_horaires_8p":1.3
+}
+
+deneigeuse_T2 = {
+    "nom" : "deneigeuse Type2",
+    "vitesse" :  20,
+    "cout_jour": 800,
+    "cout_kilo": 1.3,
+    "cout_horaires_8":1.3,
+    "cout_horaires_8p":1.5
+}
+
+def run_scenario(scenario, place, multi=None):
+    print(f'Lancement du scenario {scenario}')
     if scenario == "scan":
         if place:
             return scan(place)
-    # if scenario == "deneigement":
-    #     if place == "montreal" and not multi:
-    #         return 
+    elif scenario == "deneigement":
+        if multi is not None:
+            return calculer_meilleur_choix([deneigeuse_T1,deneigeuse_T2], multi)
+        else:
+            return deneigement_euler(place,deneigeuse_T1)
 
 
 def main():
     parser = argparse.ArgumentParser(description='ERO1 Optimisation Hivernale')
-    parser.add_argument('--scenario', choices=['scan', 'scenario2', 'scenario3'], default='scan', help='Le scénario à exécuter (par défaut: scan)')
+    parser.add_argument('--scenario', choices=['scan', 'deneigement', 'presentation'], default='scan', help='Le scénario à exécuter (par défaut: scan)')
     parser.add_argument('--place', choices=['montreal', 'outremont', 'verdun', 'riviere', 'saintLeonard', 'montRoyal'], default='montreal', help="Choix de l'endroit ou effectuer le scenario (par défaut: montreal)")
-    parser.add_argument('--multi', action='store_true', help="Exécuter le scénario de déneigement en mode multi-deneigeuse")
+    parser.add_argument('--multi', nargs='+',choices=['outremont', 'verdun', 'riviere', 'saintLeonard', 'montRoyal'] ,help="Exécuter le scénario de déneigement avec les quartiers spécifiés pour savoir laquelle choisir")
     
     args = parser.parse_args()
 
